a0bb7d55bf34d6763b93095bf732fa68b2ed49dc
Collin Jilbert <cjilbert504@gmail.com>
2020-01-07 16:30:27 -0600

    Decided to remove comments to rethink a few things clearly

--- lib/stay_koation/cli.rb
+++ lib/stay_koation/cli.rb
@@ -2,7 +2,6 @@ require "pry"
 
 class StayKoation::CLI
 
-    # This method controls the UI and the flow of the program
     def call
         welcome_image
         sleep 3
@@ -18,7 +17,6 @@ class StayKoation::CLI
         Campground.amenities_scrape
     end
 
-    # This is the method that "welcomes" the user to the program
     def welcome_image
         puts
         puts "============================"
@@ -26,7 +24,6 @@ class StayKoation::CLI
         puts "============================"
     end
 
-    # This method is responsible for instantiating a new User instance and greets the user
     def new_user_greeting
         puts
         puts "Please enter your first name:"
@@ -36,10 +33,6 @@ class StayKoation::CLI
         puts "Hello, #{user_name}!"
     end
 
-    # This method prompts the user to select a state, displays the state select menu, gets the users
-    # selection, gets the selected states url attribute and passes it to the Campground state_select
-    # method to then be used by the Campground..scrape_koa_campgrounds method; Perhaps this belongs 
-    # in the State class and probably needs to be refactored into smaller methods that only do one job
     def state_select_prompt
         puts
         puts "Please select the number of the state in which you would like to start your KOA campsite search from the following list:"
