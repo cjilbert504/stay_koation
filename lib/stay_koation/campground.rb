5d73476a92f131f168c9f3e98c5e79249986dc92
Collin Jilbert <cjilbert504@gmail.com>
2020-01-07 11:02:16 -0600

    Added comments

--- lib/stay_koation/campground.rb
+++ lib/stay_koation/campground.rb
@@ -18,20 +18,26 @@ class Campground
         @@all
     end
 
+    # This method takes in the user selection from the state_select_prompt to be used in the 
+    # scrape_koa_campgrounds method
     def self.state_select(state)
         @state_pick = state
     end
 
+    # This method scrapes the select state url to get a list of the campgrounds in that state 
+    # and parses them as needed
     def self.scrape_koa_campgrounds
         doc = Nokogiri::HTML(open("#{@state_pick}"))
         campgrounds = doc.search("div.media-heading").text
         @campgrounds_array = campgrounds.split(" KOA")
     end
 
+    # This method creates new instances of Campground from the scraped array
     def self.new_from_scrape
         @campgrounds_array.each {|x| x = self.new(x)}
     end
 
+    # This method iterates over the @@all array and creates a numbered menu with the stored elements
     def self.campground_menu
         counter = 1
         puts
@@ -41,11 +47,14 @@ class Campground
         end
     end
 
+    # This method prompts the user for a campground selection in order to view its amenities
     def self.view_amenities_selection 
         puts "Please select the number of the campground whose amenities you would like to view:"
         @view_selection = gets.strip.to_i
     end
 
+    # This method takes the users input, selects the appropriate campground from the @@all array
+    # and formats its name in order to be used to scrape its url
     # Maybe this should be an instance method
     def self.campground_name_normalize
         name = Campground.all[@view_selection-1].name
@@ -54,6 +63,7 @@ class Campground
         @amenities_url = url_name[0].downcase.gsub(" ", "-")
     end
 
+    # This method scrapes the amenities of the selected campground and displays them to the user
     # Maybe this should be an instance method
     def self.amenities_scrape
         doc = Nokogiri::HTML(open("https://koa.com/campgrounds/#{@amenities_url}/"))
